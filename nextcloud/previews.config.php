e900e463e78e28d07994591c1ac384b8bc7daf81
Thumbscrew <thumbscrw@pm.me>
2022-01-31 22:44:18 +0000

    add previews config file

---
+++ nextcloud/previews.config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array(
+    'enable_previews' => true,
+    'enabledPreviewProviders' => array(
+        0 => 'OC\\Preview\\TXT',
+        1 => 'OC\\Preview\\MarkDown',
+        2 => 'OC\\Preview\\OpenDocument',
+        3 => 'OC\\Preview\\PDF',
+        4 => 'OC\\Preview\\MSOffice2003',
+        5 => 'OC\\Preview\\MSOfficeDoc',
+        6 => 'OC\\Preview\\Image',
+        7 => 'OC\\Preview\\Photoshop',
+        8 => 'OC\\Preview\\TIFF',
+        9 => 'OC\\Preview\\SVG',
+        10 => 'OC\\Preview\\Font',
+        11 => 'OC\\Preview\\MP3',
+        12 => 'OC\\Preview\\Movie',
+        13 => 'OC\\Preview\\MKV',
+        14 => 'OC\\Preview\\MP4',
+        15 => 'OC\\Preview\\AVI',
+    )
+)
+?>
